cf3fb7771e85727a9c813e72d4df25b9b7b8f2b8
SBalint2002 <sarandi.balint@gmail.com>
2024-03-04 00:43:00 +0100

    defining computed and watched values

--- src/views/CreatePage.vue
+++ src/views/CreatePage.vue
@@ -35,16 +35,6 @@
                         v-model="linkText"
                     />
                 </div>
-                <div class="mb-3">
-                    <label for="" class="form-label">
-                        Link URL
-                    </label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        v-model="linkUrl"
-                    />
-                </div>
                 <div class="row mb-3">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" v-model="published">
@@ -60,83 +50,52 @@
         <div class="mb-3">
             <button
                 class="btn btn-dark"
-                :disabled="isFromInvalid"
+                :disabled="isFormInvalid"
                 @click.prevent="submitForm"
             >Create Page</button>
         </div>
     </form>
 </template>
+<script setup>
+import {inject, ref, computed, watch} from 'vue';
+import {useRouter} from 'vue-router';
+const bus = inject('$bus');
+const pages = inject('$pages');
+const router = useRouter();
 
-<script>
-export default {
-    emits: {
-        pageCreated({pageTitle, content, link}) {
-            if (!pageTitle) {
-                return false;
-            }
+let pageTitle = ref('');
+let content = ref('');
+let linkText = ref('');
+let published = ref(true);
 
-            if (!content) {
-                return false;
-            }
-
-            if (!link || !link.text || !link.url) {
-                return false;
-            }
-
-            return true;
-        }
-    },
-    computed: {
-        isFromInvalid() {
-            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
-        }
-    },
-    data() {
-        return {
-            pageTitle: '',
-            content: '',
-            linkText: '',
-            linkUrl: '',
-            published: true
-        }
-    },
-    methods: {
-        submitForm() {
-            if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
-                alert('Please fill the form');
-                return;
-            }
-
-            this.$emit('pageCreated', {
-                pageTitle: this.pageTitle,
-                content: this.content,
-                link: {
-                    text: this.linkText,
-                    url: this.linkUrl
-                },
-                published: this.published
-            });
-
-            this.pageTitle = '';
-            this.content = '';
-            this.linkText = '';
-            this.linkUrl = '';
-            this.published = true;
-        }
-    },
-    watch: {
-        pageTitle(newTitle, oldTitle) {
-            if (this.linkText == oldTitle) {
-                this.linkText = newTitle;
-            }
-        }
+function submitForm() {
+    if (!pageTitle || !content || !linkText) {
+        alert('Please fill the form');
+        return;
     }
+
+    let newPage = {
+        pageTitle: pageTitle.value,
+        content: content.value,
+        link: {
+            text: linkText.value,
+        },
+        published: published.value
+    };
+
+    pages.addPage(newPage)
+
+    bus.$emit('page-created', newPage);
+
+    router.push({path: '/pages'});
 }
+
+const isFormInvalid = computed(() => !pageTitle || !content || !linkText);
+
+watch(pageTitle, (newTitle, oldTitle) => {
+    if (linkText.value == oldTitle) {
+        linkText.value = newTitle;
+    }
+});
+
 </script>
-
-<!-- v-model does the same:
-
-v-model = "pageTitle"
-
-:value="pageTitle"
-@input="(e) => pageTitle = e.target.value" -->
