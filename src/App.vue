b972f2f7fadb0d883cc4af4b0be2aac685a79b1c
SBalint2002 <sarandi.balint@gmail.com>
2024-03-03 13:04:55 +0100

    updating and filtering data

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@ export default {
             this.pages = data;
         },
         pageCreated(pageObj) {
-            console.log(pageObj);
+            this.pages.push(pageObj);
         }
     }
 }
