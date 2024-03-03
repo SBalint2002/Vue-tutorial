fddb1a437f341f613bddef1e2d596390aa7e0ec1
SBalint2002 <sarandi.balint@gmail.com>
2024-03-03 22:09:32 +0100

    nesting routes

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,16 @@
     <navbar></navbar>
 
     <router-view></router-view>
-    
-    <!-- v-show hides the element is false, v-if doesn't output that element at all  -->
-    <!-- <page-viewer 
-        v-if="pages.length > 0"
-        :page="pages[activePage]"
-    ></page-viewer>
 
-    <create-page
-        @page-created="pageCreated"
-    ></create-page>
- -->
 </template>
 
 <script>
 
 import Navbar from './components/Navbar.vue';
-import PageViewer from './components/PageViewer.vue';
-import CreatePage from './components/CreatePage.vue';
 
 export default {
     components: {
-        Navbar,
-        PageViewer,
-        CreatePage
+        Navbar
     },
 
     methods: {
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,7 @@
 
                 <li>
                     <router-link 
-                        to="/create"
+                        to="/pages/create"
                         class="nav-link"
                         active-class="active"
                         aria-current="page"
