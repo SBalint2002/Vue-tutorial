71dd17fd5dc9c9483b8fb244cf82c96fa26a7ec8
SBalint2002 <sarandi.balint@gmail.com>
2024-03-03 20:13:41 +0100

    using route params

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,24 +1,15 @@
 <template>
     <div class="container">
         <h1 class="emphasize">Page Title</h1>
-        <p>Content</p>
+        <p>{{$route.params.index}}</p>
     </div>
 </template>
 
 <script>
 
 export default {
-    props: {
-        page : {
-            type: Object,
-            default(rawProps) {
-                //Ha undefined akkor ez az alapértelmezett érték
-                return {
-                    pageTitle: '',
-                    content: ''
-                };
-            }
-         }
+    created() {
+        //console.log(this.$route.params);
     }
 }
 </script>
