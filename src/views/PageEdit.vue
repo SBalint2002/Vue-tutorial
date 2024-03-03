08e2fa8a81ec734481c21931bad205affc277fa7
SBalint2002 <sarandi.balint@gmail.com>
2024-03-04 00:50:31 +0100

    delete functionality

--- src/views/PageEdit.vue
+++ src/views/PageEdit.vue
@@ -59,9 +59,13 @@
                 @click.prevent="submit"
             >Edit</button>
             <button
-                class="btn btn-secondary"
+                class="btn btn-secondary me-2"
                 @click.prevent="goToPagesList"
             >Cancel</button>
+            <button
+                class="btn btn-danger"
+                @click.prevent="deletePage"
+            >Delete</button>
         </div>
     </form>
 
@@ -92,6 +96,14 @@ function submit() {
 
 function goToPagesList() {
     router.push({path: '/pages'});
+}
+
+function deletePage() {
+    pages.removePage(index);
+
+    bus.$emit('page-deleted', {index});
+
+    goToPagesList();
 }
 
 </script>
